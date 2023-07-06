b6d58443b60ee6d85ab6fc8138fefb1d5e032321
unknown <gusrud3226@gmail.com>
2023-07-06 12:25:15 +0900

    js파일 추가및 about수정

---
+++ about/kakaoApi.js
@@ -0,0 +1,46 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+		    mapOption = {
+		        center: new kakao.maps.LatLng(37.5262,127.0355), // 지도의 중심좌표
+		        level: 3, // 지도의 확대 레벨
+		        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+		    }; 
+
+		// 지도를 생성한다 
+		var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+		// 마우스 휠과 모바일 터치를 이용한 지도 확대, 축소를 막는다
+		map.setZoomable(false);   
+
+		// 지도에 확대 축소 컨트롤을 생성한다
+		var zoomControl = new kakao.maps.ZoomControl();
+
+		// 지도의 우측에 확대 축소 컨트롤을 추가한다
+		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+		// 지도 클릭 이벤트를 등록한다 (좌클릭 : click, 우클릭 : rightclick, 더블클릭 : dblclick)
+		kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+			console.log('지도에서 클릭한 위치의 좌표는 ' + mouseEvent.latLng.toString() + ' 입니다.');
+		});	
+
+		// 지도 드래깅 이벤트를 등록한다 (드래그 시작 : dragstart, 드래그 종료 : dragend)
+		kakao.maps.event.addListener(map, 'drag', function () {
+			var message = '지도를 드래그 하고 있습니다. ' + 
+							'지도의 중심 좌표는 ' + map.getCenter().toString() +' 입니다.';
+			console.log(message);
+		});
+		// 마커 이미지의 주소
+		var markerImageUrl = 'https://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_p.png', 
+		    markerImageSize = new kakao.maps.Size(42, 44), // 마커 이미지의 크기
+		    markerImageOptions = { 
+		        offset : new kakao.maps.Point(13, 39)// 마커 좌표에 일치시킬 이미지 안의 좌표
+		    };
+
+		// 마커 이미지를 생성한다
+		var markerImage = new kakao.maps.MarkerImage(markerImageUrl, markerImageSize, markerImageOptions);
+
+		// 지도에 마커를 생성하고 표시한다
+		var marker = new kakao.maps.Marker({
+		    position: new kakao.maps.LatLng(37.5262,127.0355), // 마커의 좌표
+		    image : markerImage, // 마커의 이미지
+		    map: map // 마커를 표시할 지도 객체
+		});
---
+++ common/header.js
@@ -0,0 +1,20 @@
+//scroll-up-bar.js
+let prescroll = window.scrollY;
+console.log(prescroll);
+
+$(window).on("scroll", () => {
+  //스크롤시 변수에 저장
+  let scroll = window.scrollY;
+  console.log(scroll);
+  console.log("prescroll00", prescroll);
+
+  // 이전 스크롤값이 크면 트루, 스크루 올리면 헤더보임
+  if (prescroll > scroll) {
+    $("nav").css({ top: 0 });
+  } else {
+    // 이후 스크롤값이 크면 풜스, 스크루 올리면 헤더가려짐
+    $("nav").css({ top: -70 });
+  }
+
+  prescroll = scroll;
+});
