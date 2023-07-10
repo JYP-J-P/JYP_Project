3c9eafbc23ff760d445cff691708513e1e8f2b24
hyunk07 <gusrud3226@gmail.com>
2023-07-10 09:33:10 +0900

    index 로딩 이미지수정

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-// to top 버튼 작동
+//loading
+const html = document.querySelector("html");
+html.style.overflow = "hidden";
+$(window).on("load", function () {
+  var counter = 0;
+  var c = 0;
 
+  var i = setInterval(function () {
+    counter++;
+    c++;
+    if (counter == 101) {
+      html.style.overflow = "auto";
+      clearInterval(i);
+      $(".loading").animate({ opacity: "0" }, 400, function () {
+        $(this).css({ display: "none" });
+      });
+    }
+  }, 20);
+});
+// to top 버튼 작동
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   let submain = $('.main_text_box1').offset().top;
