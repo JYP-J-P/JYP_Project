a37d20225d9ab8f2aa0a0bad831337cd3a016ec1
hyunk07 <gusrud3226@gmail.com>
2023-07-10 15:24:42 +0900

    지도 수정

--- about/kakaoApi.js
+++ about/kakaoApi.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
   mapOption = { 
-		center: new kakao.maps.LatLng(37.5262,127.0355), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.5262, 127.0355),
         level: 4, // 지도의 확대 레벨
 		mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
     };
